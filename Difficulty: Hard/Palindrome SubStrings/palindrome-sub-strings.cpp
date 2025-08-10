8f2dbe46008e2a934e0cd6acccfc0f4eaa7b8304
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2025-08-10 12:08:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Palindrome SubStrings/palindrome-sub-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int countPS(string &s) {
+        int n = s.size();
+        int count = 0;
+                auto expand = [&](int left, int right) {
+            int localCount = 0;
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                if (right - left + 1 >= 2) 
+                    localCount++;
+                left--;
+                right++;
+            }
+            return localCount;
+        };
+                for (int i = 0; i < n; i++) {
+            count += expand(i, i);     
+            count += expand(i, i + 1); 
+        }
+        return count;
+    }
+};
