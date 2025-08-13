68fd52df40953fbe9bdc51dfb75ab2f3fc2563de
Rohit Kumar Dubey <rohitdubey0601@gmail.com>
2025-08-13 13:09:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Tywin's War Strategy/tywins-war-strategy.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int minSoldiers(vector<int>& arr, int k) {
+        int n = arr.size();
+        int required = (n + 1) / 2; // ceil(n/2)
+        int luckyCount = 0;
+        vector<int> cost;
+        
+        for (int soldiers : arr) {
+            if (soldiers % k == 0) {
+                luckyCount++;
+            } else {
+                cost.push_back(k - (soldiers % k));
+            }
+        }
+        if (luckyCount >= required) return 0;
+        sort(cost.begin(), cost.end());
+        int troopsNeeded = required - luckyCount;
+        long long totalAdd = 0;
+        for (int i = 0; i < troopsNeeded; i++) {
+            totalAdd += cost[i];
+        }
+        return totalAdd;
+    }
+};
